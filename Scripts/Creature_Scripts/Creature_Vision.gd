0a24c2cf16311a8d4437fd84fb4138f63bd514ca
Mac Klosky <macklosky21@gmail.com>
2025-06-05 11:28:14 -0400

    Added raycast functionality so that creature cannot see player through walls.

--- Scripts/Creature_Scripts/Creature_Vision.gd
+++ Scripts/Creature_Scripts/Creature_Vision.gd
@@ -2,11 +2,15 @@ extends Node
 class_name Creature_Vision
 
 @onready var mid_vision: Area3D = $Mid_Vision
+@onready var vision_cast : RayCast3D = $RayCast3D
+
 func _Check_Vision(area : Area3D) -> Array[Player_Controller]:
 	var Players_In_Vision : Array[Player_Controller]
 	for object in area.get_overlapping_bodies():
 		if object is Player_Controller:
-			Players_In_Vision.append(object)
+			vision_cast.target_position = (object.global_position - vision_cast.global_position) # These two may have to be backwards.
+			vision_cast.force_raycast_update()
+			if vision_cast.get_collider() is Player_Controller: Players_In_Vision.append(object)
 	return Players_In_Vision
 
 func _Check_All_Vision() -> Array[Player_Controller]:
