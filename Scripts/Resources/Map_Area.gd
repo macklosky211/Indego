eb1a4d863ba83df38fa4379bf34b0e96b5d0e965
Mac Klosky <macklosky21@gmail.com>
2025-06-05 11:27:10 -0400

    Created functionality for get_all_thigns() functions.

--- Scripts/Resources/Map_Area.gd
+++ Scripts/Resources/Map_Area.gd
@@ -8,8 +8,16 @@ class_name Map_Area
 @export var Navigation_Mesh : NavigationRegion3D
 
 
-#func get_all_creatures() -> Array[Monster]:
-#	pass
+func get_all_creatures() -> Array[Creature_Controller]:
+	var found_monsters : Array[Creature_Controller]
+    if Map_collider == null: push_error("Map_Collider for ", name, " was null.")
+    for children in Map_Collider.get_overlapping_bodies():
+        if child is Creature_Controller: found_monsters.append(child)
+    return found_monsters
 
-#func get_all_players() -> Array[Player]:
-#	pass
+func get_all_players() -> Array[Player_Controller]:
+    var found_players : Array[Player_Controller]
+    if Map_collider == null: push_error("Map_Collider for ", name, " was null.")
+    for children in Map_Collider.get_overlapping_bodies():
+        if child is Player_Controller: found_players.append(child)
+    return found_players
