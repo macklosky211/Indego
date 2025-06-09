e07956cbc9e89638cea37c9d11644ec8cc672c0b
Mac Klosky <macklosky211@gmail.com>
2025-06-08 21:32:38 -0400

    actual basic multiplayer implimentation

--- Scripts/Multiplayer_Scripts/player_spawner.gd
+++ Scripts/Multiplayer_Scripts/player_spawner.gd
@@ -7,9 +7,17 @@ func _ready() -> void:
 		spawn_function = player_spawn_function
 
 func player_spawn_function(vars) -> Node:
+	print("Player is being spawned in.")
 	var id = vars[0]
 	var new_player : Node = Player_Scene.instantiate()
-	new_player.position = Vector3(0,10,0)
+	
+	new_player.position = Vector3(0,20,0)
+	
 	new_player.set_multiplayer_authority(id, true)
+	
+	for child in new_player.get_children():
+		if child is Camera3D:
+			if id == multiplayer.get_unique_id(): child.make_current()
+			else: child.queue_free()
 	
 	return new_player
