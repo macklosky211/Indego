d933d9ae59d152e7a39aabaf902fe1d1ad9b7f42
Mac Klosky <macklosky21@gmail.com>
2025-06-12 11:54:31 -0400

    Moved files to local repo

---
+++ Readme.md
@@ -0,0 +1,3 @@
+## All files moved to local git repository, see @macklosky211 for access.
+
+## This repository will remain in use for the GitHub Issues functionality.
--- Scripts/Creature_Scripts/Creature_States/Creature_Idle.gd
+++
@@ -1,8 +0,0 @@
-extends Creature_State
-class_name Creature_Idle
-
-func _update(_creature : Creature_Controller, _delta : float) -> void:
-	pass
-
-func _get_state_name() -> String:
-	return "Creature_Idle"
